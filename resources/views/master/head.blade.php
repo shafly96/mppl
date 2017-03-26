81cb54786c15b05edf9c8950c57eae5156707bf0
DESKTOP-O15N8BP\shafly <shaflynaufal96@gmail.com>
2017-03-26 18:53:09 +0700

    perbaikan

--- resources/views/master/head.blade.php
+++ resources/views/master/head.blade.php
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-
-    <!-- Bootstrap 3.3.5 -->
-    <link rel="stylesheet" href="{{url('')}}/bootstrap/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="{{url('')}}/dist/css/AdminLTE.min.css">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-    folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="{{url('')}}/dist/css/skins/_all-skins.min.css">
-    <!-- iCheck -->
-    <link rel="stylesheet" href="{{url('')}}/plugins/iCheck/flat/blue.css">
-    <!-- Morris chart -->
-    <link rel="stylesheet" href="{{url('')}}/plugins/morris/morris.css">
-    <!-- jvectormap -->
-    <link rel="stylesheet" href="{{url('')}}/plugins/jvectormap/jquery-jvectormap-1.2.2.css">
-    <!-- Date Picker -->
-    <link rel="stylesheet" href="{{url('')}}/plugins/datepicker/datepicker3.css">
-    <!-- Daterange picker -->
-    <link rel="stylesheet" href="{{url('')}}/plugins/daterangepicker/daterangepicker-bs3.css">
-    <!-- bootstrap wysihtml5 - text editor -->
-    <link rel="stylesheet" href="{{url('')}}/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
-    <!-- datatables -->
-    <link href="{{url('')}}/plugins/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
-
-    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.1.1/css/responsive.bootstrap.min.css">
-    <!-- sweet alert -->
-    <link href="{{url('')}}/plugins/sweetalert/sweet-alert.css" rel="stylesheet" type="text/css" />
-
-    <!--select2-->
-    <link href="{{url('')}}/plugins/select2/select2.min.css" rel="stylesheet" type="text/css" />
-
-</head>
-=======
 <meta charset="utf-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <!-- Tell the browser to be responsive to screen width -->
@@ -69,7 +27,10 @@
 <link rel="stylesheet" href="{{url('')}}/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
 <!-- datatables -->
 <link href="{{url('')}}/plugins/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
+
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/responsive/2.1.1/css/responsive.bootstrap.min.css">
 <!-- sweet alert -->
 <link href="{{url('')}}/plugins/sweetalert/sweet-alert.css" rel="stylesheet" type="text/css" />
->>>>>>> befb4c2627618f11edf6c07d961b54e332313a93
+
+<!--select2-->
+<link href="{{url('')}}/plugins/select2/select2.min.css" rel="stylesheet" type="text/css" />
